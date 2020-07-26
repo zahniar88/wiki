aceb2d91146effcb76c151bf63a16cff6608440a
Zahniar Adirahman <zahniar88@gmail.com>
2020-07-26 23:42:34 +0700

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,22 +3,16 @@ module.exports = {
   base: '/',
   dest: 'public',
   description: 'Litle Creator',
-  logo: './assets/img/logo.png',
+  logo: 'https://scontent.fplm3-1.fna.fbcdn.net/v/t1.0-9/70638228_125227068850913_4921359444006141952_n.jpg?_nc_cat=106&_nc_sid=09cbfe&_nc_eui2=AeEqk9m6_NB-yhHrYmsm2Uu7cjCMybOXz5NyMIzJs5fPk3zh9rN3kbM9E54ffwhX867PRU1uRXqv4rUKOZc-SH4q&_nc_ohc=95hUSkmb4s0AX-3z8KO&_nc_ht=scontent.fplm3-1.fna&oh=2856249966e188df55769add3eac74c3&oe=5F4322E4',
   theme: require.resolve('../../'),
   themeConfig: {
   authors: [
       {
       name: 'Zahniar Adirahman',
       avatar: '/assets/img/sal.jpg',
-      link: 'https://wowthemes.net/donate',
-      linktext: 'Follow',
-      },
-      {
-        name: 'John Doe',
-        avatar: '/assets/img/avatar.png',
-        link: 'https://bootstrapstarter.com/',
-        linktext: 'Follow',
-      },
+      link: 'https://www.instagram.com/ziqo.space/',
+      linktext: 'Visit',
+      }
     ],
     footer: {
       contact: [
