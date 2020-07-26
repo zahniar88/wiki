d5172a0dc33cba06c0830994f8932c412b29a6c4
Zahniar Adirahman <zahniar88@gmail.com>
2020-07-27 00:27:21 +0700

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -26,7 +26,7 @@ module.exports = {
         },
         {
           type: 'github',
-          link: 'https://github.com/wowthemesnet/mediumish-vuepress-blog-theme',
+          link: 'https://github.com/zahniar88',
         },
         {
           type: 'gitlab',
