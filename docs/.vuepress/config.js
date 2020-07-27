6584edad556a13baccc02aff3d3bd89c1cdd9f78
Zahniar Adirahman <zahniar88@gmail.com>
2020-07-27 11:46:02 +0700

    add nikipedia disqus

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: 'NikiPedia',
   base: '/',
   dest: 'public',
-  description: 'Litle Creator',
+  description: 'NikiPedia Indonesia',
   logo: 'https://scontent.fplm3-1.fna.fbcdn.net/v/t1.0-9/70638228_125227068850913_4921359444006141952_n.jpg?_nc_cat=106&_nc_sid=09cbfe&_nc_eui2=AeEqk9m6_NB-yhHrYmsm2Uu7cjCMybOXz5NyMIzJs5fPk3zh9rN3kbM9E54ffwhX867PRU1uRXqv4rUKOZc-SH4q&_nc_ohc=95hUSkmb4s0AX-3z8KO&_nc_ht=scontent.fplm3-1.fna&oh=2856249966e188df55769add3eac74c3&oe=5F4322E4',
   theme: require.resolve('../../'),
   themeConfig: {
@@ -78,7 +78,7 @@ module.exports = {
     },
     comment: {
       service: 'disqus',
-      shortname: 'demowebsite',
+      shortname: 'nikipedia',
     },
     newsletter: {
       endpoint: 'https://wowthemes.us11.list-manage.com/subscribe/post?u=8aeb20a530e124561927d3bd8&id=8c3d2d214b'
