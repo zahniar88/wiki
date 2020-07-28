1f167b7e3383f9de3f3fc5fa88b0bba1de757e67
Zahniar Adirahman <zahniar88@gmail.com>
2020-07-28 13:03:04 +0700

    add search box

--- global-components/BaseListLayout.vue
+++ global-components/BaseListLayout.vue
@@ -1,90 +1,99 @@
 <template>
-<div id="">
+  <div id="">
+    <div class="d-md-none">
+      <SearchBox class="home-saerch mb-4" />
+    </div>
 
-<div class="card-panel-content row" itemscope itemtype="http://schema.org/Blog">
+    <div
+      class="card-panel-content row"
+      itemscope
+      itemtype="http://schema.org/Blog"
+    >
+      <article
+        v-for="page in pages"
+        :key="page.key"
+        itemprop="blogPost"
+        class="ui-post entry col-md-4 card-container"
+        itemscope
+        itemtype="https://schema.org/BlogPosting"
+      >
+        <meta itemprop="mainEntityOfPage" :content="page.path" />
+        <div class="entry card h-100">
+          <div class="card-inside">
+            <div class="card-subheading">
+              <div
+                v-if="page.frontmatter.tags"
+                class="ui-post-meta ui-post-tag"
+                itemprop="keywords"
+              >
+                <router-link
+                  v-for="tag in resolvePostTags(page.frontmatter.tags)"
+                  :key="tag"
+                  :to="'/tag/' + tag"
+                >
+                  {{ tag }}
+                </router-link>
+              </div>
+            </div>
+            <h2 class="heading" itemprop="name headline">
+              <NavLink :link="page.path" class="heading">{{
+                page.title
+              }}</NavLink>
+            </h2>
+            <p itemprop="description">
+              {{ page.frontmatter.summary || page.summary }}
+            </p>
+          </div>
+          <div class="meta-bottom mt-auto">
+            <div v-if="$themeConfig.authors">
+              <span
+                v-for="author in $themeConfig.authors"
+                :key="author.name"
+                class="nav-item"
+              >
+                <div class="d-flex">
+                  <a class="profile-avatar">
+                    <img
+                      v-if="author.name === page.frontmatter.author"
+                      :src="$withBase(author.avatar)"
+                      class="avatar-image"
+                      :alt="author.name"
+                    />
+                  </a>
+                  <div class="meta">
+                    <div v-if="author.name === page.frontmatter.author">
+                      <span class="username">{{ author.name }}</span> &nbsp;
+                    </div>
+                    <div></div>
+                  </div>
+                </div>
+              </span>
+            </div>
 
-<article
-v-for="page in pages"
-:key="page.key"
-itemprop="blogPost"
-class="ui-post entry col-md-4 card-container"
-itemscope
-itemtype="https://schema.org/BlogPosting"
->
-  <meta itemprop="mainEntityOfPage" :content="page.path" />
-  <div class="entry card h-100">
-
-    <div class="card-inside">
-      <div class="card-subheading">
-        <div
-        v-if="page.frontmatter.tags"
-        class="ui-post-meta ui-post-tag"
-        itemprop="keywords"
-        >
-        <router-link
-        v-for="tag in resolvePostTags(page.frontmatter.tags)"
-        :key="tag"
-        :to="'/tag/' + tag"
-        >
-        {{ tag }}
-        </router-link>
+            <div v-if="page.frontmatter.date" class="date">
+              <time
+                pubdate
+                itemprop="datePublished"
+                :datetime="page.frontmatter.date"
+              >
+                {{ resolvePostDate(page.frontmatter.date) }}
+              </time>
+            </div>
+          </div>
         </div>
-      </div>
-      <h2 class="heading" itemprop="name headline">
-       <NavLink :link="page.path" class="heading">{{ page.title }}</NavLink>
-      </h2>
-      <p itemprop="description">{{ page.frontmatter.summary || page.summary }}</p>
+      </article>
     </div>
-    <div class="meta-bottom mt-auto">
 
-
-      <div v-if="$themeConfig.authors">
-      <span
-      v-for="author in $themeConfig.authors"
-      :key="author.name"
-      class="nav-item"
-      >
-      <div class="d-flex">
-      <a class="profile-avatar">
-      <img v-if="author.name === page.frontmatter.author" :src="($withBase)(author.avatar)" class="avatar-image" :alt="author.name">
-      </a>
-      <div class="meta">
-      <div  v-if="author.name === page.frontmatter.author">
-      <span class="username">{{author.name}}</span> &nbsp;
-      </div>
-      <div>
-      </div>
-      </div>
-      </div>
-      </span>
-      </div>
-
-      <div v-if="page.frontmatter.date" class="date"><time
-      pubdate
-      itemprop="datePublished"
-      :datetime="page.frontmatter.date"
-      >
-      {{ resolvePostDate(page.frontmatter.date) }}
-      </time> </div>
-
-
-
-    </div>
+    <component
+      :is="paginationComponent"
+      v-if="$pagination.length > 1 && paginationComponent"
+    ></component>
   </div>
-</article>
-</div>
-
-<component
-:is="paginationComponent"
-v-if="$pagination.length > 1 && paginationComponent"
-></component>
-</div>
-
 </template>
 
 <script>
 /* global THEME_BLOG_PAGINATION_COMPONENT */
-
+import SearchBox from '@SearchBox'
 import Vue from 'vue'
 import dayjs from 'dayjs'
 import {
@@ -93,7 +102,9 @@ import {
 } from '@vuepress/plugin-blog/lib/client/components'
 
 export default {
-
+  components: {
+    SearchBox,
+  },
 
   data() {
     return {
@@ -159,4 +170,15 @@ export default {
   margin-top: -20px;
 .username
   margin-top:-10px;
+.home-saerch
+  width 100%;
+  z-index 100000;
+.home-saerch > input
+  width 100% !important;
+@media screen and (max-width: 768px)
+  .home-saerch > input
+    left 0;
+@media screen and (max-width: 959px)
+  .home-saerch > input
+    border-color $borderColor;
 </style>
